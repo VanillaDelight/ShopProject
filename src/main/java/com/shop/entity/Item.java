3cee53f78d0b9919f0d6b5a5898bd0a907488c74
VanillaDelight <2m5@naver.com>
2022-11-02 21:41:35 +0900

    study

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -8,16 +8,16 @@ import lombok.ToString;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "item")
+@Entity // 클래스를 엔티티로 선언
+@Table(name = "item") // 엔티티와 매핑할 테이블을 지정
 @Getter
 @Setter
 @ToString
 public class Item {
 
-    @Id
-    @Column(name = "item_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id // 테이블의 기본키에 사용할 속성을 지정
+    @Column(name = "item_id") // 필드와 컬럼 매핑
+    @GeneratedValue(strategy = GenerationType.AUTO) // 키 값을 생성하는 전략 명시(AUTO 가 기본값 JPA 구현체가 자동으로 전략 결정)
     private Long id; // 상품 코드
 
     @Column(nullable = false, length = 50)
@@ -29,11 +29,14 @@ public class Item {
     @Column(nullable = false)
     private int stockNumber; // 재고수량
 
+    // BLOB, CLOB 타입 매핑
+    // B => 바이너리 데이터를 DB 외부에 저장하기 위함 (이미지, 사운드, 비디오 같은 멀티미디어 데이터)
+    // C => 사이즈가 큰 데이터를 외부파일로 저장하기 위함 (문자형 대용량 파일)
     @Lob
     @Column(nullable = false)
     private String itemDetail; // 상품 상세 설명
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) // enum 타입 매핑
     private ItemSellStatus itemSellStatus; // 상품 판매 상태
 
     private LocalDateTime regTime; // 등록시간

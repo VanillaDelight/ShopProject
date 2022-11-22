45385130bbd307916a621e6d01bf02cb243de807
VanillaDelight <2m5@naver.com>
2022-11-22 22:55:55 +0900

    study

--- src/test/java/com/shop/entity/CartTest.java
+++ src/test/java/com/shop/entity/CartTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @Transactional
 @TestPropertySource(locations = "classpath:application-test.properties")
-public class CartTest {
+class CartTest {
 
     @Autowired
     CartRepository cartRepository;
